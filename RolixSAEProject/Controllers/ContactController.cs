3be5b0e4c0cf93869bc10b78e56f155304cc5740
Fatih746 <fatih.yildirim@etu.univ-smb.fr>
2025-12-19 14:32:56 +0100

    finis la partie formaulire + traduction en cours

---
+++ RolixSAEProject/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RolixSAEProject.Models;
+using RolixSAEProject.Services;
+
+namespace RolixSAEProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly DataverseService _dataverseService;
+
+        public ContactController(DataverseService dataverseService)
+        {
+            _dataverseService = dataverseService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // On précise "Contact" car ton fichier ne s'appelle pas Index.cshtml
+            return View("Contact");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Submit(ContactForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                var leadData = new Dictionary<string, object>
+                {
+                    { "firstname", model.FirstName ?? "" },
+                    { "lastname", model.LastName ?? "" },
+                    { "emailaddress1", model.EMailAddress1 ?? "" },
+                    { "subject", model.Message ?? "Nouveau message Web" }, // Rubrique
+                    { "description", model.Description ?? "" },
+                    { "leadsourcecode", 8 } // Source Web
+                };
+
+                // CORRECTION : "lead" sans le "s" pour Dataverse
+                await _dataverseService.CreateEntity("lead", leadData);
+
+                return RedirectToAction("Index", "Home");
+            }
+            // En cas d'erreur, on réaffiche bien la vue Contact
+            return View("Contact", model);
+        }
+    }
+}
