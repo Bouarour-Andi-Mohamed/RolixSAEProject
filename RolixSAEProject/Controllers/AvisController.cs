53dc29b07074ec53aa5d23bf0fc73e2869936c17
Ilyes Ikadarne <Ilyes.Ikadarne@etu.univ-smb.fr>
2026-01-23 15:22:39 +0100

    Travail sur l'avis du client lié à sa commande est fait

---
+++ RolixSAEProject/Controllers/AvisController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RolixSAEProject.Models;
+
+namespace RolixSAEProject.Controllers
+{
+    public class AvisController : Controller
+    {
+        // Affiche la page pour saisir l'avis
+        [HttpGet]
+        public IActionResult Create(string orderId)
+        {
+            // On lie l'avis à la commande cliquée
+            var model = new AvisViewModel { OrderId = orderId };
+            return View(model);
+        }
+
+        // Action qui reçoit les données du formulaire (Note et Commentaire)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AvisViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Ici se fera plus tard l'envoi vers ta table Dataverse
+                return RedirectToAction("Index", "Account");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ RolixSAEProject/Models/AvisViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RolixSAEProject.Models
+{
+    public class AvisViewModel
+    {
+        public string OrderId { get; set; }
+        public int Note { get; set; } // Pour ta colonne "Nombre entier"
+        public string Commentaire { get; set; } // Pour ta colonne "Texte"
+    }
+}
